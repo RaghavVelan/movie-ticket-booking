eb279b9b7f2098b8d39c3d0fa85dde72cf5e85c5
Raghav Velan <vraghav950@gmail.com>
2022-06-22 16:07:49 +0530

    commit

--- src/component/Cinema.js
+++ src/component/Cinema.js
@@ -1,67 +1,46 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import './Cinema.css';
-
+import './style.css';
+import App_logo from '../icon/Icon@2x.png';
+import thor_trailer from '../Videos/thor_trailer.mp4';
 
 class Cinema extends React.Component {
-
-
-
-
     render() {
         return (
             <div>
-                <div className='container-fluid' >
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <h3>React Bootstrap Carousel</h3>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12">
-                            <Carousel>
+                <Carousel>
+  <Carousel.Item>
+  <video autoPlay >
+      <source src={thor_trailer} type="video/mp4"/>
+</video>
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+  <video width="100%" height="80%" controls >
+      <source src={thor_trailer} type="video/mp4"/>
+</video>
 
-                                <Carousel.Item>
-                                    <img
-                                        className="d-block w-100"
-                                        src="https://picsum.photos/500/300?img=1"
-                                        alt="First slide"
-                                    />
-                                    <Carousel.Caption>
-                                        <h3>First slide label</h3>
-                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+  <video width="100%" height="80%" controls >
+      <source src={thor_trailer} type="video/mp4"/>
+</video>
 
-                                <Carousel.Item>
-                                    <img
-                                        className="d-block w-100"
-                                        src="https://picsum.photos/500/300?img=2"
-                                        alt="Second slide"
-                                    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
 
-                                    <Carousel.Caption>
-                                        <h3>Second slide label</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-
-                                <Carousel.Item>
-                                    <img
-                                        className="d-block w-100"
-                                        src="https://picsum.photos/500/300?img=3"
-                                        alt="Third slide"
-                                    />
-                                    <Carousel.Caption>
-                                        <h3>Third slide label</h3>
-                                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-
-                            </Carousel>
-                        </div>
-                    </div>
-                </div>
+               
             </div>
         )
     };
