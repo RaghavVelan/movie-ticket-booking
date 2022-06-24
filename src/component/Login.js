a0c6e84e613265f3c08bc5e3f7963e6da81f509b
Raghav Velan <vraghav950@gmail.com>
2022-06-24 16:10:59 +0530

    commit

--- src/component/Login.js
+++ src/component/Login.js
@@ -15,30 +15,19 @@ function Login(){
                     <div className="boxb">
                         <div id="btn"></div>
                         <button className="rl button2" onClick="login()">Login</button>
-                        <button className="rl button2" onClick="register()">Register</button>
                     </div>
                     <form className="ig" id="Login">
                         <input type="text" className="if" placeholder='User ID' required/>
                         <input type="password" className="if" placeholder='Enter Password' required/>
-                        <label for="remember" className="remember"><input type="checkbox" name="remember" id="remember" className='check-box' />Remember Password</label>
+                        <br />
+                        {/* <label for="remember" className="remember"><input type="checkbox" name="remember" id="remember" className='check-box' />Remember Password</label> */}
                         <button type="button" className="submit">Login</button>
+                        <br />
                         <h4 id='h4'>OR</h4>
-                        <Link to='/signup'>New User Click Here</Link>   
+                        <Link to='/Signup' id='ancher' className='user-login-signin'>New User Click Here</Link>   
                      
                     </form>
-                    <form id="register" className="ig1">
-                        <input type="text" className="if" placeholder='User ID' required/>
-                        <input type="email" className="if" placeholder='Email ID' required/>
-                        <input type="password" className="if" placeholder='Enter Password' required/>
-                        <button type="button" className="submit">Register</button>
-                        <h4>OR</h4>
-                        <div className="link-btn">
-                            <a href="#" className="b2">
-                                <img src={glogo} width="22px" /> Google</a>
-                            <a href="#" className="b3">
-                                <img src={alogo} width="22px" /> iCloud</a>
-                        </div> 
-                    </form>
+                    
                 </div>
             </div>
         </div>
--- src/component/signup.jsx
+++ src/component/signup.jsx
@@ -8,15 +8,33 @@ import alogo from '../image/apple.png';
 
 function signup(){
   return (<>
-    <div className='form-sign'>
+  <div className='form-sign'>
         <div className="card middle">
             <div className="front">
                 <div className="content">
-                    <form id="register" className="ig1">
+                    <div className="boxb">
+                        <div id="btn"></div>
+                        {/* <button className="rl button2" onClick="login()">Login</button> */}
+                        <button className="rl button2" onClick="register()">Register</button>
+                    </div>
+                    {/* <form className="ig" id="Login">
+                        <input type="text" className="if" placeholder='User ID' required/>
+                        <input type="password" className="if" placeholder='Enter Password' required/>
+                        <label for="remember" className="remember"><input type="checkbox" name="remember" id="remember" className='check-box' />Remember Password</label>
+                        <button type="button" className="submit">Login</button>
+                        <h4 id='h4'>OR</h4>
+                        <Link to='/Signup'>New User Click Here</Link>   
+                     
+                    </form> */}
+                    <form id="register" className="ig">
                         <input type="text" className="if" placeholder='User ID' required/>
                         <input type="email" className="if" placeholder='Email ID' required/>
                         <input type="password" className="if" placeholder='Enter Password' required/>
-                        <button type="button" className="submit">Register</button>                     
+                        <button type="button" className="submit">Register</button>
+                        <br />
+                        <h4>OR</h4>
+                        <br />
+                        <Link to='/Login' id='ancher' className='user-login-signin'>Already have an Account? Click Here</Link>   
                     </form>
                 </div>
             </div>
