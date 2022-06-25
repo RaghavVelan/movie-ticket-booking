90972d7dece4f514d942734b74f8e28d8de6d9f4
username <username@cognizant.com>
2022-06-25 21:37:13 +0530

    commit

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,8 +1,56 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './style.css';
+import blank from '../image/blank.jpg'
 
 const  Contact = () => {
-  return (<h1>contact us</h1>);
+  return (<div>
+
+
+<h1 className='contact'>Contact The Team.</h1>
+
+<div class="proof">
+  <img src={blank}/>
+  <div class="overlay">
+    Raghav
+  </div>
+</div>
+
+
+<div class="proof">
+  <img src={blank}/>
+  <div class="overlay">
+    Amritpal Singh
+  </div>
+</div>
+
+
+<div class="proof">
+  <img src={blank}/>
+  <div class="overlay">
+    Shivansh
+  </div>
+</div>
+
+
+<div class="proof">
+  <img src={blank}/>
+  <div class="overlay">
+    Shalini
+  </div>
+</div>
+
+
+<div class="proof">
+  <img src={blank}/>
+  <div class="overlay">
+    Navya
+  </div>
+</div>
+
+
+  </div>
+);
 };
 
 export default Contact;
