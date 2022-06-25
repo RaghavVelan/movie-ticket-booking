8d46ee80ce477fad734b0b895213efff86abaf7d
Raghav Velan <vraghav950@gmail.com>
2022-06-25 15:30:01 +0530

    commit

--- src/component/Cinema.js
+++ src/component/Cinema.js
@@ -1,8 +1,20 @@
 import React from 'react';
-// import { Carousel } from 'react-bootstrap';
 import './style.css';
 import App_logo from '../icon/Icon@2x.png';
 import thor_trailer from '../Videos/thor_trailer.mp4';
+import dune from '../image/dune.png'
+import dr_strange from '../image/doctor-strange.png'
+import rainfall from '../image/rainfall.png'
+import avenger from '../image/avenger.png'
+import toy_story from '../image/toy-story-4.png'
+import joker from '../image/joker.png'
+import lion_king from '../image/the-lion-king.png'
+import free_guy from '../image/free-guy.png'
+import jurassic from '../image/jurassic-world-dominion.png'
+import thor from '../image/thor.png'
+import popeye from '../image/popeye.png'
+import batman from '../image/batman.png'
+
 
 class Cinema extends React.Component {
     render() {
@@ -14,37 +26,45 @@ class Cinema extends React.Component {
                 </video>
                 <h3 id='movie-title'>Thor Love and Thunder</h3>
               </div>
-                {/* <Carousel>
-  <Carousel.Item>
-  <video autoPlay >
-      <source src={thor_trailer} type="video/mp4"/>
-</video>
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  <video width="100%" height="80%" controls >
-      <source src={thor_trailer} type="video/mp4"/>
-</video>
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel> */}
-
-               
+                <h4 className='current-shows'>Show Timing</h4>
+              <div className="movie-show">
+                <div className="show-timing">
+                  <img src={thor} alt="Thor Love and Thunder"/>
+                  <div className="timing">
+                    <buttom className="single-show-timing btn btn-danger">10AM-1PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">2PM-5PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">5PM-8PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">8PM-11PM</buttom>
+                  </div>
+                </div>  
+                <div className="show-timing">
+                  <img src={popeye} alt="popeye"/>
+                  <div className="timing">
+                    <buttom className="single-show-timing btn btn-danger">10AM-1PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">2PM-5PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">5PM-8PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">8PM-11PM</buttom>
+                  </div>
+                </div> 
+                <div className="show-timing">
+                  <img src={rainfall} alt="rainfall"/>
+                  <div className="timing">
+                    <buttom className="single-show-timing btn btn-danger">10AM-1PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">2PM-5PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">5PM-8PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">8PM-11PM</buttom>
+                  </div>
+                </div> 
+                <div className="show-timing">
+                  <img src={dune} alt="dune"/>
+                  <div className="timing">
+                    <buttom className="single-show-timing btn btn-danger">10AM-1PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">2PM-5PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">5PM-8PM</buttom>
+                    <buttom className="single-show-timing btn btn-danger">8PM-11PM</buttom>
+                  </div>
+                </div> 
+              </div>               
             </div>
         )
     };
